780369fc26dea5ababb4069cc77aa2441103c683
-Izuki- <ggph640@gmail.com>
2025-04-13 15:54:10 +0700

    Кнопка удаления элемента
    Добавлена кнопка удаления конкретного элемента из списка
    
    +Небольшое исправление внешнего вида кода

--- src/Components/myPohuyList/Main-Page/Main-page.js
+++ src/Components/myPohuyList/Main-Page/Main-page.js
@@ -7,14 +7,19 @@ function MainPage() {
     const addNewItem = () => {
         const userValue = prompt('Введите имя для нового элемента списка:')
         if (userValue) {
-            setItems([...items, { 
+            setItems([...items, {
+            id: Date.now(),
             value: userValue}]);
         }
     };
 
-    const deleteItems = () => {
-        setItems(items.filter(items => items !== items));
+    const deleteAllItems = () => {
+        setItems([]);
     }
+
+    const deleteItem = (id) => {
+        setItems(prevItems => prevItems.filter(item => item.id !== id));
+    };
 
     function message() {
         alert("ПОХУЙ")
@@ -28,22 +33,21 @@ function MainPage() {
                     <div key={item.id} className="main-page__item">
                         <input type='checkbox'/>
                         <div className='item-value'>{item.value}</div>
+                        <button className='deleteItemButton' type='button'
+                        onClick={() => deleteItem(item.id)}></button>
                     </div>
                 ))}
             </form>
             <div className='button-section'>
-                <button type="button"
-                onClick={addNewItem}>
+                <button type="button" onClick={addNewItem}>
                     Добавить
                 </button>
 
-                <button type='button'
-                onClick={deleteItems}>
+                <button type='button' onClick={deleteAllItems}>
                     Очистить
                 </button>
 
-                <button type='button' 
-                onClick={message}>
+                <button type='button' onClick={message}>
                     Похуй
                 </button>
             </div>
